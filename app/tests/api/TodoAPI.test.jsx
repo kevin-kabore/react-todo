1ee550efc78094a977b8436da4eeaffb39b5c3cb
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-01-06 14:48:06 -0800

    Adds filter functionality and tests

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -57,4 +57,48 @@ describe('TodoAPI', () => {
 
     })
   })
+
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: 'some filter Todo 1',
+      completed: true
+    },
+    {
+      id: 2,
+      text: 'Some filter Todo 2',
+      completed: false
+    },
+    {
+      id: 3,
+      text: 'filter Todo 3',
+      completed: true
+    }];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return non-complted items if showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by compltedStatus', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '')
+      expect(filteredTodos[0].completed).toBe(false)
+    });
+
+    it('should filter todos by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should return all todos if searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+  })
 })
