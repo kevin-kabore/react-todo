81006eec15e8384dc3eec822e54ed7eebd034ec4
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-02-03 20:21:42 -0800

    Adds react-router middleware and redirect on auth

--- app/app.jsx
+++ app/app.jsx
@@ -1,13 +1,20 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {hashHistory} = require('react-router');
 
 var actions = require('actions');
-var store = require('configureStore').configure()
-var TodoAPI = require('TodoAPI')
-import Login from 'Login';
-import TodoApp from 'TodoApp';
+var store = require('configureStore').configure();
+import firebase from 'app/firebase/';
+import router from 'app/router/';
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    hashHistory.push('/todos');
+  } else {
+    hashHistory.push('/')
+  }
+});
 
 store.dispatch(actions.startAddTodos())
 // load foundation
@@ -16,15 +23,24 @@ $(document).foundation();
 // load app css
 require('style!css!sass!applicationStyles')
 
+var requireLogin = (nextState, replace, next) => {
+  if (!firebase.auth().currentUser) {
+    replace('/');
+  }
+  next();
+};
+
+var redirectIfLoggedIn = (nextState, replace, next) => {
+  if (firebase.auth().currentUser) {
+    replace('/todos')
+  }
+  next()
+};
+
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/">
-        <Route path="todos" component={TodoApp}/>
-        <IndexRoute component={Login}/>
-      </Route>
-    </Router>
-  </Provider>,
+    {router}
+  </Provider>,  
 
   document.getElementById('app')
 );
